b8fe13edb0b6a4bfad827a01dbb61bfeed30a7d8
碗碗 <43742453+Surbowl@users.noreply.github.com>
2020-03-22 18:52:15 +0800

    使用 bool 替代 int，节约内存
    使用 bool 替代 int，节约内存

--- BasicLevel_C++/1093. 字符串A+B (20).cpp
+++ BasicLevel_C++/1093. 字符串A+B (20).cpp
@@ -2,13 +2,13 @@
 using namespace std;
 int main() {
     string s1, s2, s;
-    int hash[200] = {0};
+    bool hash[127] = {false};
     getline(cin, s1);
     getline(cin, s2);
     s = s1 + s2;
     for (int i = 0; i < s.size(); i++) {
-        if (hash[s[i]] == 0) cout << s[i];
-        hash[s[i]] = 1;
+        if (! hash[s[i]]) cout << s[i];
+        hash[s[i]] = true;
     }
     return 0;
 }
